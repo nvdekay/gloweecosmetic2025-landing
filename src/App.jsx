af6b520e97a6cc1965e5c74cb0a0e9b6623a289d
nvdekay <khanhnvd.work@gmail.com>
2025-10-23 17:33:24 +0700

    feat: config git

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-import React from 'react'
+import React from "react";
+import Navbar from "./components/Navbar";
 
 function App() {
-
   return (
     <>
-      <h1 className='font-bold'>Hello, World!</h1>
+      <Navbar />
+      <h1 className="font-bold">Hello, World!</h1>
     </>
-  )
-} 
+  );
+}
 
-export default App
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <div>
+      <nav className="navbar text-9xl text-amber-800 font-extrabold">
+        <h1>My Website</h1>
+      </nav>
+    </div>
+  );
+}
